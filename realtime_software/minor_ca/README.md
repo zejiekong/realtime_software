ab38b01709867ceb6184a56e55583a401c6d03ab
Ze Jie <ze-jie-kong-@hotmail.com>
2023-02-19 16:08:23 +0800

    minor_ca

---
+++ realtime_software/minor_ca/README.md
@@ -0,0 +1,22 @@
+# Minor Programming Assignment
+
+## Assignement
+You are tasked to write a program to calculate the roots of a quadratic equation.
+The program will request for the appropriate parameters, from the user, for arguments a, b and c. Your program will, subsequently analyse for real and equal, real and different and for complex roots. It will also check for correctness of parameters entered and require a re-entry should input be inappropriate. The results of the values and type of roots of the equation are to be displayed  and tabulated as well as the form of the quadratic equation, listed. 
+
+## Project Requirement
+* Check for correctness of input parameters 
+* Analyse the type of roots of the quadratic equation 
+* Display quadratic equation 
+* List the values of the roots  
+* Classify the conditions of the roots (real, complex, same, different) 
+
+## Additional Requirements
+### Real-time check of data entered, via keyboard.  
+* Check validity of input values and for any missing data (as required) 
+* Help & suggestion in identification of input error.  
+### Software robustness 
+* Stable operation and avoidance of infinite loop or programming “hanging” 
+### Display of Data
+* Pleasant format of output results showing data points entered and required classification of data entered. 
+* Preferably the data entered and computed would be presented in a neat table or “graphical” format.  
--- realtime_software/minor_ca/minor_ca.c
+++ realtime_software/minor_ca/minor_ca.c
@@ -1,1 +1,90 @@
-#include <minor_ca.h>
+// TO-DO : DISPLAY IN TABLE , should simplify equation?
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+float a,b,c;
+struct roots
+{
+    float x1;
+    float x2;
+    float real;
+    float im;
+}root;
+
+enum root_type{complex=0,equal=1,different=2};
+
+void keyboard_input()
+{
+    printf("Please input a\n");
+    while (!scanf("%f",&a))
+    {
+        printf("Please input a again\n");
+        while(getchar()!='\n');
+    }
+    printf("Please input b\n");
+    while (!scanf("%f",&b))
+    {
+        printf("Please input b again\n");
+        while(getchar()!='\n');
+    }
+    printf("Please input c\n");
+    while (!scanf("%f",&c))
+    {
+        printf("Please input c again\n");
+        while(getchar()!='\n');
+    }
+    printf("%.3fx^2 + %.3fx + %.3f",a,b,c);
+}
+
+enum root_type quadratic_roots()
+{
+    float discriminant = b*b -4*a*c; 
+    if(discriminant < 0)
+    {
+        printf("Complex Roots\n");
+        root.real = -b / 2*a;
+        root.im = sqrtf(-discriminant)/2*a;
+        return complex; 
+    }
+    else if (discriminant == 0)
+    {
+        printf("Equal Real Roots\n");
+        root.x1 = (-b+sqrtf(discriminant))/2*a;
+        root.x2 = (-b-sqrtf(discriminant))/2*a;
+        return equal;
+    }
+    else if (discriminant > 0)
+    {
+        printf("Different Real Roots\n");
+        root.x1 = (-b+sqrtf(discriminant))/2*a;
+        root.x2 = (-b-sqrtf(discriminant))/2*a;
+        return different;
+    }
+}
+
+void display_table()
+{
+
+}
+
+int main()
+{
+    printf("Start of Program\n");
+    keyboard_input();
+    switch(quadratic_roots())
+    {
+        case complex:
+            printf("x1: %.3f+%.3fi\nx2: %.3f-%.3fi\n",root.real,root.im,root.real,root.im);
+            break;
+        case equal:
+            printf("x1: %.3f\nx2: %.3f\n",root.x1,root.x2);
+            break;
+        case different:
+            printf("x1: %.3f\nx2: %.3f\n",root.x1,root.x2);
+            break;
+    }
+    display_table();
+    exit(0);
+}
